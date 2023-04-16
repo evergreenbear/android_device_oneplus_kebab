cf917b9e143a6ff752761c8377cd7b48ce657d24
Collin M <mojippt@gmail.com>
2023-04-16 11:51:46 -0600

    Volume panel on left by default

--- device.mk
+++ device.mk
@@ -22,11 +22,6 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
-## Manually added-in apps
-# KernelSU: for Stellaris kernel
-PRODUCT_PACKAGES += \
-    KernelSU
-
 # Device init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
